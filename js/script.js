ba6ccaabbbdc9a67186db575811a99086bd7eb2b
Sergey <sergey636@yandex.ru>
2022-04-12 00:07:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use srtict";
+
+let number = 5;
+const leftBorderWidth =1;
+
+number=10;
+console.log(number);
+
+
+
+console.log(name);
+var name = 'Ivan';
+
+
+const answer = prompt("Сколько вам лет?", "18");
+console.log(answer);
